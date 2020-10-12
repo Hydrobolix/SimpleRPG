2fccd6fb7a3316fda78bced30f6e255cc458ded9
Hydrobolix <Hydrobolix@DESKTOP-5VN67BH>
2020-10-11 20:43:12 -0500

    Fixed critical bug

--- game.js
+++ game.js
@@ -8,7 +8,6 @@ var combat = document.getElementById("combat");
 var combatInfo = document.getElementById("combatInfo");
 var atkBtn = document.getElementById("atk");
 var skillDesc = document.getElementById("skillDesc");
-var NotAnsemTheWise1 = document.getElementById();
 
 menu.style.visibility = "hidden";
 atkBtn.style.visibility = "hidden";
